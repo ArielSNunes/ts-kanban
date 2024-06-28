92fb57ceb922d677ca36f9097aaa26c5a97e4bde
Ariel dos Santos Nunes <arielsn1@gmail.com>
2024-06-28 13:57:01 -0300

    feat: updated code to get data from db

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import pgPromise from 'pg-promise';
 
 const app = express();
+const connection = pgPromise()('postgres://Arielzito:c6t6dsfa9876sbd65cf976absdcf987@localhost:5432/');
 
-app.get('/boards', function (req, res) {
-    const boards = [
-        { name: 'Projeto 01' }
-    ]
+app.get('/boards', async function (req, res) {
+    const boards = await connection.query('select * from branas_kanban.boards', []);
     return res.json(boards);
 })
 
